3715d28c849b3533fed61f7342f50b5a301c7e95
Jorge Andres Gutierrez Chavez <guti800500@siigo.com>
2022-04-19 10:26:57 -0500

    Singleton first part

---
+++ DesingPattern/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Singleton
+{
+    public class Singleton
+    {
+        //readonly: es solo para lectura, solo se puede leer y no editarlo
+        //static: pertenece a la clase y no al objeto
+        private readonly static Singleton _instance = new Singleton(); //se crea el objeto una ves se compile el proyecto
+
+        //Creamos el acceso al objeto _instance con la siguiente propiedad o atributo
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        //creamos un constructor privado para que no se puedan crear objetos de esta clase
+        private Singleton() { }
+    }
+}
